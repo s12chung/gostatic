f66c27c1b434d2b51d9f33ee0261c0f57e7743f9
Steve Chung <steve.chung7@gmail.com>
2018-08-24 15:23:39 -0400

    make blueprint content a bit more organize

--- blueprint/go/content/content.go
+++ blueprint/go/content/content.go
@@ -1,44 +1,26 @@
 package content
 
 import (
-
 	"github.com/sirupsen/logrus"
 
 	"github.com/s12chung/gostatic/go/app"
+	"github.com/s12chung/gostatic/go/lib/html"
 	"github.com/s12chung/gostatic/go/lib/router"
 	"github.com/s12chung/gostatic/go/lib/webpack"
-	"github.com/s12chung/gostatic/go/lib/html"
 )
 
 type Content struct {
 	Settings *Settings
 	Log      logrus.FieldLogger
 
-	HtmlRenderer      *html.Renderer
-	Webpack  *webpack.Webpack
+	HtmlRenderer *html.Renderer
+	Webpack      *webpack.Webpack
 }
 
 func NewContent(generatedPath string, settings *Settings, log logrus.FieldLogger) *Content {
 	w := webpack.NewWebpack(generatedPath, settings.Webpack, log)
-	htmlRenderer := html.NewRenderer(settings.Html, []html.Plugin{ w }, log)
-	return &Content{ settings, log, htmlRenderer, w }
-}
-
-func (content *Content) RenderHtml(ctx router.Context, name, defaultTitle string, data interface{}) error {
-	bytes, err := content.HtmlRenderer.Render(name, defaultTitle, data)
-	if err != nil {
-		return err
-	}
-	return ctx.Respond(bytes)
-}
-
-func (content *Content) SetRoutes(r router.Router, tracker *app.Tracker) {
-	r.GetRootHTML(func(ctx router.Context) error {
-		return content.RenderHtml(ctx, "root", "", "Hello World!")
-	})
-	r.GetHTML("/404.html", func(ctx router.Context) error {
-		return content.RenderHtml(ctx, "404", "404", nil)
-	})
+	htmlRenderer := html.NewRenderer(settings.Html, []html.Plugin{w}, log)
+	return &Content{settings, log, htmlRenderer, w}
 }
 
 func (content *Content) WildcardUrls() ([]string, error) {
@@ -51,4 +33,25 @@ func (content *Content) AssetsUrl() string {
 
 func (content *Content) GeneratedAssetsPath() string {
 	return content.Webpack.GeneratedAssetsPath()
+}
+
+func (content *Content) RenderHtml(ctx router.Context, name, defaultTitle string, data interface{}) error {
+	bytes, err := content.HtmlRenderer.Render(name, defaultTitle, data)
+	if err != nil {
+		return err
+	}
+	return ctx.Respond(bytes)
+}
+
+func (content *Content) SetRoutes(r router.Router, tracker *app.Tracker) {
+	r.GetRootHTML(content.getRoot)
+	r.GetHTML("/404.html", content.get404)
+}
+
+func (content *Content) getRoot(ctx router.Context) error {
+	return content.RenderHtml(ctx, "root", "", "Hello World!")
+}
+
+func (content *Content) get404(ctx router.Context) error {
+	return content.RenderHtml(ctx, "404", "404", nil)
 }
